765c8b353cac0acf63ef5a80df368fde3d7c6f2b
vxxjonathanxxv <jonathan.vorm@aon.at>
2019-09-24 13:17:30 +0200

    fourth commit(working on Beispiel 3)

--- HÜ2_Beispiel3/src/NumberTester.java
+++ HÜ2_Beispiel3/src/NumberTester.java
@@ -1,8 +1,34 @@
 public class NumberTester {
-    public NumberTester(String fileName){}
-    public void setOddEvenTester(NumberTest oddTester){}
-    public void setPrimeTester(NumberTest primeTester){}
-    public void setPalindromeTester(NumberTest palindromeTester){}
-    public void testFile(){}
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester){
+
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester){
+
+    }
+
+    public void testFile(){
+        NumberTest oddTester = (n) -> (n%2) == 0;
+        setOddEvenTester(oddTester);
+
+        NumberTest primeTester = (k) -> {
+            for(int i =k-1; i > 1; i--){
+                if(k%i == 0) return true;
+            }
+            return false;
+        };
+        setPrimeTester(primeTester);
+
+        //NumberTest palindromeTester = (l) ->
+
+    }
 
 }
