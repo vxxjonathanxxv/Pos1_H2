efb8aeb12310ceac080194a71313235d3fd90633
vxxjonathanxxv <jonathan.vorm@aon.at>
2019-09-24 12:42:55 +0200

    third commit start with Beispiel3

---
+++ HÜ2_Beispiel3/src/NumberTest.java
@@ -0,0 +1,2 @@
+public interface NumberTest {
+    boolean testNumber(int number);
---
+++ HÜ2_Beispiel3/src/NumberTester.java
@@ -0,0 +1,8 @@
+public class NumberTester {
+    public NumberTester(String fileName){}
+    public void setOddEvenTester(NumberTest oddTester){}
+    public void setPrimeTester(NumberTest primeTester){}
+    public void setPalindromeTester(NumberTest palindromeTester){}
+    public void testFile(){}
+
+}
