0eae611c8001ae9e821ece2579b2e0f018225959
vxxjonathanxxv <jonathan.vorm@aon.at>
2019-09-24 12:15:35 +0200

    second commit Beispiel 2 is finished

--- HÜ2_Beispiel2/src/HalloJavamitForEach.java
+++ HÜ2_Beispiel2/src/HalloJavamitForEach.java
@@ -1,6 +1,30 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class HalloJavamitForEach {
-    List<String> list = new ArrayList<>();
+    List<String> arr;
+
+    public static void main(String[] args){
+        HalloJavamitForEach h = new HalloJavamitForEach();
+        h.maketheList();
+        h.normalGoThrough();
+    }
+
+    public void maketheList(){
+        arr = new ArrayList<>();
+        arr.add("Ahja");
+        arr.add("B");
+        arr.add("C");
+        arr.add("D");
+    }
+
+    private void forEachGOThrough(){
+        arr.forEach(a-> System.out.println(a));
+    }
+
+    private void normalGoThrough(){
+        for(String s : arr) System.out.println(s);
+    }
 }
